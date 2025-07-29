58f082a24c6b1a19b4cd380a68d429d8f884527f
Joaquin Peralta <jperaltac@gmail.com>
2025-07-29 14:05:49 -0400

    Add GPLv3 headers

--- MLGeneral/codes/code1.py
+++ MLGeneral/codes/code1.py
@@ -1,3 +1,7 @@
+#
+# This file is part of the erpi5 project and is licensed under the
+# GNU General Public License v3.  See the LICENSE file for details.
+#
 from tensorflow.keras import layers, Sequential
 
 model = Sequential([
